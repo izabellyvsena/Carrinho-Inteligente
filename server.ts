5403602d17f7029853190112b8a49f3724c1b2b9
izabellyvsena <izabellyvsena@gmail.com>
2026-06-15 18:54:43 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -65,8 +65,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// Porta do Render (obrigatória)
-const PORT = process.env.PORT || 3000;
+// Porta do Render (obrigatória)const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`PechinchaBot rodando na porta ${PORT}`);
 });
+
